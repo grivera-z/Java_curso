4d0f8bb2f61591510e54bc3e6307b4d4e3d6f332
Rivera <gabriel.ale.rivera-morales@dxc.com>
2019-02-12 17:10:33 -0600

    Estructuras

---
+++ EstructurasJava/src/Estructuras_Listas_Enlazadas/Lista.java
@@ -0,0 +1,126 @@
+package Estructuras_Listas_Enlazadas;
+
+public class Lista {
+
+    private Nodo cabeza=null;
+    private int longitud = 0;
+
+    private class Nodo {
+        public Libro libro;
+        public Nodo siguiente = null;
+
+        public Nodo(Libro libro){
+            this.libro = libro;
+        }
+    }
+
+    public void insertarPrincipio(Libro libro){
+        Nodo nodo =  new Nodo(libro);
+        nodo.siguiente = cabeza;
+        cabeza = nodo;
+        longitud++;
+    }
+
+    public void insertarFinal(Libro libro){
+        Nodo nodo = new Nodo(libro);
+        if(cabeza==null){
+            cabeza=nodo;
+        }
+        else {
+            Nodo puntero = cabeza;
+            while (puntero.siguiente != null) {
+                puntero = puntero.siguiente;
+            }
+            puntero.siguiente = nodo;
+        }
+        longitud++;
+    }
+    public void insertarDespues(int n, Libro libro){
+        Nodo nodo = new Nodo(libro);
+        if(cabeza==null){
+            cabeza=nodo;
+        }else{
+            Nodo puntero = cabeza;
+            int contador = 0;
+            while (contador<n && puntero.siguiente != null) {
+                puntero = puntero.siguiente;
+                contador++;
+            }
+            nodo.siguiente = puntero.siguiente;
+            puntero.siguiente = nodo;
+        }
+        longitud++;
+    }
+    public Libro obtener(int n){
+        if(cabeza==null){
+            return null;
+        }else{
+            Nodo puntero = cabeza;
+            int contador = 0;
+            while(contador<n && puntero.siguiente !=null){
+                puntero = puntero.siguiente;
+                contador++;
+            }
+            if(contador != n ){
+                return null;
+            }
+            else{
+                return puntero.libro;
+            }
+        }
+    }
+
+    public int Contador(){
+        return longitud;
+    }
+    public boolean estaVacia(){
+        return cabeza==null;
+    }
+
+    public void eliminarPrimero(){
+        if(cabeza!=null){
+            Nodo primer = cabeza;
+            cabeza = cabeza.siguiente;
+            primer.siguiente = null;
+            longitud--;
+        }
+    }
+
+    public void eliminarFinal() {
+        if (cabeza != null) {
+            if(cabeza.siguiente == null){
+                cabeza = null;
+            }else {
+                Nodo puntero = cabeza;
+                while (puntero.siguiente.siguiente != null) {
+                    puntero = puntero.siguiente;
+                }
+                puntero.siguiente = null;
+            }
+            longitud--;
+        }
+    }
+    public void eliminarLibro(int n){
+        if (cabeza != null) {
+            if(cabeza.siguiente == null){
+                Nodo primer = cabeza;
+                cabeza = cabeza.siguiente;
+                primer.siguiente = null;
+                longitud--;
+            }else if (n<longitud){
+                Nodo puntero = cabeza;
+                int contador = 0;
+                while (contador<(n-1)) {
+                    puntero = puntero.siguiente;
+                    contador++;
+                }
+                Nodo temp = puntero.siguiente;
+                puntero.siguiente = temp.siguiente;
+                temp.siguiente = null;
+            }
+            longitud--;
+
+        }
+    }
+
+}
---
+++ EstructurasJava/src/Estructuras_Listas_Enlazadas/ListasLigadasSimples_M.java
@@ -0,0 +1,26 @@
+package Estructuras_Listas_Enlazadas;
+
+import java.util.List;
+
+public class ListasLigadasSimples_M {
+    public static void main(String[] args) {
+        Libro l1 =  new Libro("1","1","1");
+        Libro l2 =  new Libro("2","2","2");
+        Libro l3 =  new Libro("3","3","3");
+        Libro l4 =  new Libro("4","4","4");
+        Libro l5 =  new Libro("5","5","5");
+
+        Lista lis1 = new Lista();
+        lis1.insertarFinal(l1);
+        lis1.insertarFinal(l2);
+        lis1.insertarFinal(l3);
+        lis1.insertarFinal(l4);
+        lis1.insertarFinal(l5);
+
+
+        System.out.println(lis1.obtener(2).toString());
+        System.out.println("lis1.Contador() = " + lis1.Contador());
+
+
+    }
+}
