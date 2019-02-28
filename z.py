d02c7b3b8081925b45f623630a384a848e104f98
rivergab <gabriel.rivera.cib@gmail.com>
2019-02-28 01:26:28 -0600

    Test

--- z.py
+++ z.py
@@ -1,5 +1,15 @@
 values=[[1,2,3,4], [5,6,7,8], [9,10,11,12], [13,14,15,16]]
 
-for i in range(len(values)):
-    for j in range(len(values[i])):
-        print(values[i][j])
+# for i in range(len(values)):
+#     for j in range(len(values[i])):
+#         print(values[i][j])
+i=(len(values)*len(values[0])-1)
+j=0
+while(i>0):
+    for
+
+
+
+    j += 1
+    i -= 1
+
