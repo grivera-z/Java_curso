d9f23c2088773168105d81059fe7e8ca96bf9724
Rivera <gabriel.ale.rivera-morales@dxc.com>
2019-03-07 16:05:32 -0600

    las version

--- EstructurasJava/src/Main.java
+++ EstructurasJava/src/Main.java
@@ -1,5 +1,11 @@
+import static java.lang.System.out;
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] Public) {
+        String myFirstString = "Hello";
+        myFirstString.concat(" From Oracle");
+        System.out.println(myFirstString.concat(" From Oracle"));
 
+        //Let's see what we got
+        System.out.println(myFirstString);
     }
 }
